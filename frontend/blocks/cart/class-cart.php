9c7fd345e788c61bcd9d8c699bfeb84b92a79915
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-06-02 23:16:05 +0300

    Cart/Checkout loaders

--- frontend/blocks/cart/class-cart.php
+++ frontend/blocks/cart/class-cart.php
@@ -45,12 +45,46 @@ class Cart extends Dynamic {
 	 */
 	public function styles(): string {
 		$inline = '';
-		// $inline .= Frontend::get_loading_css(
-		// 	'.wc-block-cart--is-loading :where(
-		// 		.wc-block-cart-items,
-		// 		.wc-block-components-totals-wrapper
-		// 	)'
-		// );
+		$inline .= Frontend::get_loading_css(
+			'.wp-block-woocommerce-cart.is-loading :where(
+				.wp-block-woocommerce-cart-line-items-block,
+				.wp-block-woocommerce-cart-cross-sells-block,
+				.wp-block-woocommerce-cart-express-payment-block,
+				.wp-block-woocommerce-cart-accepted-payment-methods-block,
+				.wp-block-woocommerce-cart-order-summary-block div[class*="wp-block-woocommerce-cart-order-summary-"]
+			)'
+		);
+		$inline .= '
+			.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-filled-cart-block {
+				display: flex;
+				flex-wrap: wrap;
+				gap: calc(2 * var(--wp--custom--gutter));
+			}
+			.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-cart-items-block {
+				flex: 1 1 auto;
+			}
+			.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-cart-totals-block {
+				flex: 0 0 100%;
+			}
+			.wp-block-woocommerce-cart.is-loading :where(
+				.wp-block-woocommerce-cart-express-payment-block,
+				.wp-block-woocommerce-cart-accepted-payment-methods-block
+			) {
+				min-height: 2.5rem;
+			}
+			.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-cart-line-items-block {
+				min-height: 205px;
+			}
+			.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-empty-cart-block {
+				display: none;
+			}
+			@media (min-width: 991px) {
+				.wp-block-woocommerce-cart.is-loading .wp-block-woocommerce-cart-totals-block {
+					flex: 0 0 30%;
+				}
+			}
+		';
+		
 		$inline .= '
 			.wc-block-cart__totals-title.wc-block-cart__totals-title {
 				margin-top: 1rem;
