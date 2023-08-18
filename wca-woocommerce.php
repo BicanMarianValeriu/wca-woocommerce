af8ae6f1d6a9260d9c1a89284a30ebb488a7d69d
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2023-08-18 18:10:49 +0300

    wc require

--- wca-woocommerce.php
+++ wca-woocommerce.php
@@ -24,7 +24,7 @@
  * Domain Path:       /languages
  * Requires at least:       6.3
  * Requires PHP:            7.4
- * WC requires at least:    7.0
+ * WC requires at least:    8.0
  * WC tested up to:         8.0
  */
 namespace WCA\EXT\WOO;
