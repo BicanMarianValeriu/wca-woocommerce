1263f76868b9e91deffc24cab2f5549f2a65a022
Bican Marian Valeriu <marianvaleriubican@gmail.com>
2026-02-20 00:42:27 +0200

    SS

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -115,6 +115,7 @@ class Frontend {
 		wp_deregister_style( 'wc-blocks-editor-style' );
 		wp_deregister_style( 'wc-blocks-style-all-products' ); 				// Not needed
 		wp_deregister_style( 'wc-blocks-style-product-rating-stars' ); 		// Not needed
+		wp_deregister_style( 'woocommerce-blocktheme' );					// Not needed
 		wp_deregister_style( 'woocommerce-add-to-cart-form-style' ); 		// Not needed
 		wp_deregister_style( 'woocommerce-add-to-cart-with-options-style' );// Not needed
 		wp_deregister_style( 'woocommerce-product-button-style' ); 			// Not needed 
